937b3bfb86c8791f75516c1989eff1e40f27f387
Gabri432 <gabrielegatti432@gmail.com>
2022-09-06 17:45:03 +0200

    Using javascript to simplifying listing all library functions.

---
+++ docs.js
@@ -0,0 +1,22 @@
+class Formula {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+}
+
+const acceleration = Formula("Acceleration(force, mass float64) (value float64, measurementUnit string)", "")
+const angularFrequency = Formula("", "")
+const centripetalAcceleration = Formula("", "")
+const centripetalForce = Formula("", "")
+const density = Formula("", "")
+const dopplerCloser = Formula("", "")
+const dopplerFarer = Formula("", "")
+const driftSpeed = Formula("", "")
+const electricField = Formula("", "")
+const electricPotentialDiff = Formula("", "")
+const electricPotentialEnergyDiff = Formula("", "")
+const electricPotential = Formula("", "")
+
+
+function loadFormulas()
