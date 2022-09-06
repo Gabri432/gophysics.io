4e74f9568c532a3f7180cd94e4c0a99d81e77707
Gabri432 <gabrielegatti432@gmail.com>
2022-09-06 11:52:56 +0200

    Setting up basic website structure for 'gophysics.io'.

--- router.go
+++ router.go
@@ -11,25 +11,24 @@ type HttpResponseJson struct{}
 type multiplexer map[string]http.Handler
 
 type WebPage struct {
-	TitlePage string `json:"titlePage"`
+	TitlePage  string
+	htmlToLoad []string
 }
 
 var mux = multiplexer{
-	"/":        WebPage{"gophysics.io"},
-	"/about":   WebPage{"About"},
-	"/docs/":   WebPage{"Gophysics documentation"},
-	"/contact": WebPage{"Contact information"},
+	"/":        WebPage{"gophysics.io", []string{"index.html"}},
+	"/about":   WebPage{"About", []string{"templates/about.html", "templates/header.html", "templates/footer.html"}},
+	"/docs":    WebPage{"Gophysics documentation", []string{"templates/docs.html", "templates/header.html", "templates/footer.html"}},
+	"/contact": WebPage{"Contact information", []string{}},
 }
 
 func main() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/contact", contact)
-	http.HandleFunc("/docs", docs)
-	log.Fatal(http.ListenAndServe("https://gabri432.github.io/gophysics.io/", mux))
+	//https://gabri432.github.io/gophysics.io/
+	log.Fatal(http.ListenAndServe("https://gabri432.github.io/gophysics.io", mux))
 }
 
 func (m multiplexer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(m[r.RequestURI])
 	if handler, ok := m[r.RequestURI]; ok {
 		handler.ServeHTTP(w, r)
 	} else {
@@ -38,28 +37,12 @@ func (m multiplexer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (wP WebPage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch wP.TitlePage {
-	case "/":
-		homePage(w, r)
-	case "/about":
-		about(w, r)
-	case "/docs":
-		docs(w, r)
-	case "/contact":
-		contact(w, r)
-	default:
-		errorPage(w, r)
-	}
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	myTemplates := []string{"index.html", "templates/header.html", "templates/footer.html"}
-	htmlPage, err := template.ParseFiles(myTemplates...)
+	htmlPage, err := template.ParseFiles(wP.htmlToLoad...)
 	if err != nil {
+		log.Println(err)
+		errorPage(w, r)
 	}
 	htmlPage.Execute(w, HttpResponseJson{})
 }
-func about(w http.ResponseWriter, r *http.Request)     {}
-func contact(w http.ResponseWriter, r *http.Request)   {}
-func docs(w http.ResponseWriter, r *http.Request)      {}
+
 func errorPage(w http.ResponseWriter, r *http.Request) {}
