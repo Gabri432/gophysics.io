2c519ab88e97cc20d5bac520d5f95113194b8776
Gabri432 <gabrielegatti432@gmail.com>
2022-09-07 22:50:34 +0200

    Adding unique ids for functions and constants.

--- routes/docs/docs.js
+++ routes/docs/docs.js
@@ -15,73 +15,73 @@ class Constant {
 }
 
 // List of all functions
-const Acceleration = new Formula("Acceleration(force, mass float64)", "force/mass.", "m/s^2")
-const AngularFreq = new Formula("AngularFreq(time float64)", "(2 * greekPi) / time", "rad/s")
-const CentripetalAccel = new Formula("CentripetalAccel(speed, radius float64)", "(speed^2) / radius", "m/s^2")
-const CentripetalForce = new Formula(" CentripetalForce(mass, speed, radius float64)", "(mass * speed^2) / radius", "N")
-const Density = new Formula("Density(weight, volume float64)", "weight/volume", "kg/m^3")
+const Acceleration = new Formula("Acceleration (force, mass float64)", "force/mass.", "m/s^2")
+const AngularFreq = new Formula("AngularFreq (time float64)", "(2 * greekPi) / time", "rad/s")
+const CentripetalAccel = new Formula("CentripetalAccel (speed, radius float64)", "(speed^2) / radius", "m/s^2")
+const CentripetalForce = new Formula(" CentripetalForce (mass, speed, radius float64)", "(mass * speed^2) / radius", "N")
+const Density = new Formula("Density (weight, volume float64)", "weight/volume", "kg/m^3")
 // Making some space
-const DopplerCloser = new Formula("DopplerCloser(speed, frequence float64)", "(1 + (speed / 340)) * frequence", "hertz")
-const DopplerFarer = new Formula("DopplerFarer(speed, frequence float64)", "(1 - (speed / 340)) * frequence", "hertz")
-const DriftSpeed = new Formula("DriftSpeed(intensity, chargeCarrierDensity, area, particleCharge float64)", "intensity / (chargeCarrierDensity * area * particleCharge)", "m/s")
-const ElectricField = new Formula("ElectricField(charge, radius float64)", "(charge) / (4 * greekPi * Vacuum Premittivity constant * radius^2)", "N/C")
+const DopplerCloser = new Formula("DopplerCloser (speed, frequence float64)", "(1 + (speed / 340)) * frequence", "hertz")
+const DopplerFarer = new Formula("DopplerFarer (speed, frequence float64)", "(1 - (speed / 340)) * frequence", "hertz")
+const DriftSpeed = new Formula("DriftSpeed (intensity, chargeCarrierDensity, area, particleCharge float64)", "intensity / (chargeCarrierDensity * area * particleCharge)", "m/s")
+const ElectricField = new Formula("ElectricField (charge, radius float64)", "(charge) / (4 * greekPi * Vacuum Premittivity constant * radius^2)", "N/C")
 // Making some space
-const ElectricPotentDiff = new Formula("ElectricPotentDiff(charge, distance1, distance2 float64)", "(charge / (4 * greekPi * Vacuum Premittivity constant)) * ((1 / distance1) - (1 / distance2))", "V")
-const ElectricPotentEnergyDiff = new Formula("ElectricPotentEnergyDiff(charge1, charge2, distance1, distance2 float64)", "[(charge1 * charge2) / (4 * greekPi * Vacuum Premittivity constant)] * ((1 / distance1) - (1 / distance2))", "J")
-const ElectricPotential = new Formula("ElectricPotential(charge, distance float64)", "(charge) / (4 * greekPi * Vacuum Premittivity constant * distance)", "V")
-const EnergyDens = new Formula("EnergyDens(electricFieldModule float64)", "(1 / 2) * Vacuum Premittivity constant * (electricFieldModule * electricFieldModule)", "J/m^3")
-const EnergyDensity = new Formula("EnergyDensity(electricField, magneticField float64)", "(1/2)*Dieletric constant on vacuum*(electricField*electricField) + (1/(2*Vacuum Premittivity constant))*(magneticField*magneticField)", "J/m^3")
-const EscapeSpeed = new Formula("EscapeSpeed(mass, radius float64)", "[(2*G*mass)/(radius)]^(1/2)", "m/s")
+const ElectricPotentDiff = new Formula("ElectricPotentDiff (charge, distance1, distance2 float64)", "(charge / (4 * greekPi * Vacuum Premittivity constant)) * ((1 / distance1) - (1 / distance2))", "V")
+const ElectricPotentEnergyDiff = new Formula("ElectricPotentEnergyDiff (charge1, charge2, distance1, distance2 float64)", "[(charge1 * charge2) / (4 * greekPi * Vacuum Premittivity constant)] * ((1 / distance1) - (1 / distance2))", "J")
+const ElectricPotential = new Formula("ElectricPotential (charge, distance float64)", "(charge) / (4 * greekPi * Vacuum Premittivity constant * distance)", "V")
+const EnergyDens = new Formula("EnergyDens (electricFieldModule float64)", "(1 / 2) * Vacuum Premittivity constant * (electricFieldModule * electricFieldModule)", "J/m^3")
+const EnergyDensity = new Formula("EnergyDensity (electricField, magneticField float64)", "(1/2)*Dieletric constant on vacuum*(electricField*electricField) + (1/(2*Vacuum Premittivity constant))*(magneticField*magneticField)", "J/m^3")
+const EscapeSpeed = new Formula("EscapeSpeed (mass, radius float64)", "[(2*G*mass)/(radius)]^(1/2)", "m/s")
 // Making some space
-const Force = new Formula("Force(mass, acceleration float64)", "mass*acceleration", "N")
-const Frequency = new Formula("Frequency(speed, distance float64)", "speed / distance", "hertz")
-const GaussFlux = new Formula("GaussFlux(charge float64)", "charge / Vacuum Premittivity constant", "N*m^2/C")
-const GravAttract = new Formula("GravAttract(mass1, mass2, distance float64)", "(G * mass1 * mass2) / (distance^2)", "m/s^2")
-const GravField = new Formula("GravField(mass, distance float64)", "(constants.G * mass) / (distance^2)", "m/s^2")
-const HeatFlux = new Formula("HeatFlux(thermalConductivityConstant, startingTemperatureKelvin, finalTemperatureKelvin, distance float64)", "-1 * thermalConductivityConstant * (finalTemperatureKelvin - startingTemperatureKelvin) / distance", "W")
-const Intensity = new Formula("Intensity(power, area float64)", "power/area", "W/m^2")
+const Force = new Formula("Force (mass, acceleration float64)", "mass*acceleration", "N")
+const Frequency = new Formula("Frequency (speed, distance float64)", "speed / distance", "hertz")
+const GaussFlux = new Formula("GaussFlux (charge float64)", "charge / Vacuum Premittivity constant", "N*m^2/C")
+const GravAttract = new Formula("GravAttract (mass1, mass2, distance float64)", "(G * mass1 * mass2) / (distance^2)", "m/s^2")
+const GravField = new Formula("GravField (mass, distance float64)", "(constants.G * mass) / (distance^2)", "m/s^2")
+const HeatFlux = new Formula("HeatFlux (thermalConductivityConstant, startingTemperatureKelvin, finalTemperatureKelvin, distance float64)", "-1 * thermalConductivityConstant * (finalTemperatureKelvin - startingTemperatureKelvin) / distance", "W")
+const Intensity = new Formula("Intensity (power, area float64)", "power/area", "W/m^2")
 // Making some space
-const JouleHeating = new Formula("JouleHeating(resistance, currentIntensity float64)", "resistance * (currentIntensity * currentIntensity)", "W")
-const KineticEner = new Formula("KineticEner(mass, speed float64)", "(1 / 2) * mass * (speed * speed)", "J")
-const LawCoulomb = new Formula("LawCoulomb(charge1, charge2, distance float64)", "COULOMB constant * (|charge1| * |charge2|) / (distance^2)", "N")
-const LawGayLussacPressure = new Formula("LawGayLussacPressure(pressure, celsiusDeg_Temperature float64)", "pressure * (1 + (1/273)*(celsiusDeg_Temperature))", "Pascal")
-const LawGayLussacVolume = new Formula("LawGayLussacVolume(volume, celsiusDeg_Temperature float64)", "volume * (1 + (1/273)*(celsiusDeg_Temperature))", "m^3")
+const JouleHeating = new Formula("JouleHeating (resistance, currentIntensity float64)", "resistance * (currentIntensity * currentIntensity)", "W")
+const KineticEner = new Formula("KineticEner (mass, speed float64)", "(1 / 2) * mass * (speed * speed)", "J")
+const LawCoulomb = new Formula("LawCoulomb (charge1, charge2, distance float64)", "COULOMB constant * (|charge1| * |charge2|) / (distance^2)", "N")
+const LawGayLussacPressure = new Formula("LawGayLussacPressure (pressure, celsiusDeg_Temperature float64)", "pressure * (1 + (1/273)*(celsiusDeg_Temperature))", "Pascal")
+const LawGayLussacVolume = new Formula("LawGayLussacVolume (volume, celsiusDeg_Temperature float64)", "volume * (1 + (1/273)*(celsiusDeg_Temperature))", "m^3")
 // Making some space
-const LawHagenPoiseuille = new Formula("LawHagenPoiseuille(fluidViscosity, pipeLength, flowRate, pipeRadius float64)", "((8 * fluidViscosity * pipeLength * flowRate) / (greekPi * pipeRadius^4))", "Pascal")
-const LawOhm1 = new Formula("LawOhm1(resistance, currentIntensity float64)", "resistance * currentIntensity", "V")
-const LawOhm2 = new Formula("LawOhm2(resistivity, length, area float64)", "resistivity * length / area", "Ohm")
-const LawStokes = new Formula("LawStokes(fluidViscosity, radius, speed float64)", "6 * greekPi * fluidViscosity * radius * speed", "N")
-const LorentzFactor = new Formula("LorentzFactor(speed float64)", "1/{[1-(speed^2)/(C^2)]^(1/2)}", "None")
+const LawHagenPoiseuille = new Formula("LawHagenPoiseuille (fluidViscosity, pipeLength, flowRate, pipeRadius float64)", "((8 * fluidViscosity * pipeLength * flowRate) / (greekPi * pipeRadius^4))", "Pascal")
+const LawOhm1 = new Formula("LawOhm1 (resistance, currentIntensity float64)", "resistance * currentIntensity", "V")
+const LawOhm2 = new Formula("LawOhm2 (resistivity, length, area float64)", "resistivity * length / area", "Ohm")
+const LawStokes = new Formula("LawStokes (fluidViscosity, radius, speed float64)", "6 * greekPi * fluidViscosity * radius * speed", "N")
+const LorentzFactor = new Formula("LorentzFactor (speed float64)", "1/{[1-(speed^2)/(C^2)]^(1/2)}", "None")
 // Making some space
-const MechanicalEner = new Formula("MechanicalEner(potential, kinetic float64)", "potential + kinetic", "J")
-const Momentum = new Formula("Momentum(mass, speed float64)", "mass * speed", "kg*m/s")
-const NeatHeatEnergyTransfer = new Formula("NetHeatEnergyTransfer(thermalConductivityConstant, area, kelvinDeg_HeatVariation, time, width float64)", "-1 * (thermalConductivityConstant * area * kelvinDeg_HeatVariation * time) / (width)", "J")
-const NormalForce = new Formula("NormalForce(mass, angleInDeg float64)", "mass * Cosine(angleInDeg/radiant)", "N")
-const PendulumPeriod = new Formula("PendulumPeriod(pendulumLength float64)", "(2 * greekPi) * (pendulumLength/G)^(1/2)", "s")
-const PhotoElettricEffect = new Formula(" PhotoElettricEffect(frequence float64)", "Planck constant * frequence", "J")
+const MechanicalEner = new Formula("MechanicalEner (potential, kinetic float64)", "potential + kinetic", "J")
+const Momentum = new Formula("Momentum (mass, speed float64)", "mass * speed", "kg*m/s")
+const NeatHeatEnergyTransfer = new Formula("NetHeatEnergyTransfer (thermalConductivityConstant, area, kelvinDeg_HeatVariation, time, width float64)", "-1 * (thermalConductivityConstant * area * kelvinDeg_HeatVariation * time) / (width)", "J")
+const NormalForce = new Formula("NormalForce (mass, angleInDeg float64)", "mass * Cosine(angleInDeg/radiant)", "N")
+const PendulumPeriod = new Formula("PendulumPeriod (pendulumLength float64)", "(2 * greekPi) * (pendulumLength/G)^(1/2)", "s")
+const PhotoElettricEffect = new Formula(" PhotoElettricEffect (frequence float64)", "Planck constant * frequence", "J")
 // Making some space
-const PotentialElasticEner = new Formula("PotentialElasticEner(elasticConstant, distance float64)", "(1 / 2) * elasticConstant * (distance^2)", "J")
-const PotentialEner = new Formula("PotentialEner(mass, acceleration, height float64)", "mass * acceleration * height", "J")
-const PotentialGravEner = new Formula("PotentialGravEner(mass, height float64)", "mass * g * height", "J")
-const PotentialGravEner2 = new Formula("PotentialGravEner2(mass1, mass2, distance float64)", "-1 * ((constants.G * mass1 * mass2) / (distance))", "J")
-const Power = new Formula("Power(work, time float64)", "work / time", "W")
+const PotentialElasticEner = new Formula("PotentialElasticEner (elasticConstant, distance float64)", "(1 / 2) * elasticConstant * (distance^2)", "J")
+const PotentialEner = new Formula("PotentialEner (mass, acceleration, height float64)", "mass * acceleration * height", "J")
+const PotentialGravEner = new Formula("PotentialGravEner (mass, height float64)", "mass * g * height", "J")
+const PotentialGravEner2 = new Formula("PotentialGravEner2 (mass1, mass2, distance float64)", "-1 * ((constants.G * mass1 * mass2) / (distance))", "J")
+const Power = new Formula("Power (work, time float64)", "work / time", "W")
 // Making some space
-const Pressure = new Formula("Pressure(normalForce, area float64)", "normalForce / area", "Pascal")
-const ProjectileFlightTime = new Formula("ProjectileFlightTime(initialVelocity, angleInDeg float64)", "2 * initialVelocity * Sine(angleInDeg/1 radiant)", "s")
-const ProjectileMaxHeight = new Formula("ProjectileMaxHeight(initialVelocity, angleInDeg float64)", "(initialVelocity^2 * SineSquare(angleInDeg/1 radiant)) / (2 * G)", "m")
-const ProjectileMaxRange = new Formula("ProjectileMaxRange(initialVelocity, angleInDeg float64)", "(initialVelocity^2 * SineSquare(angleInDeg/1 radiant))", "m")
-const RelativDist = new Formula("RelativDist(nonTravelerDistance, speed float64)", "nonTravelerDistance/Y (Y=Lorentz factor)", "m")
-const RelativMass = new Formula("RelativMass(travelerMass, speed float64)", "mass*Y (Y=Lorentz factor)", "kg")
-const RelativMomentum = new Formula("RelativMomentum(travelerMomentum, speed float64)", "(travelerMomentum * speed)*Y (Y=Lorentz factor)", "kg*m/s")
-const RelativTime = new Formula("RelativTime(travelerTime, speed float64)", "travelerTime/Y (Y=Lorentz factor)", "s")
-const Resistance = new Formula("Resistance(resistance, currentIntensity float64)", "see 'JouleHeating'", "W")
+const Pressure = new Formula("Pressure (normalForce, area float64)", "normalForce / area", "Pascal")
+const ProjectileFlightTime = new Formula("ProjectileFlightTime (initialVelocity, angleInDeg float64)", "2 * initialVelocity * Sine(angleInDeg/1 radiant)", "s")
+const ProjectileMaxHeight = new Formula("ProjectileMaxHeight (initialVelocity, angleInDeg float64)", "(initialVelocity^2 * SineSquare(angleInDeg/1 radiant)) / (2 * G)", "m")
+const ProjectileMaxRange = new Formula("ProjectileMaxRange (initialVelocity, angleInDeg float64)", "(initialVelocity^2 * SineSquare(angleInDeg/1 radiant))", "m")
+const RelativDist = new Formula("RelativDist (nonTravelerDistance, speed float64)", "nonTravelerDistance/Y (Y=Lorentz factor)", "m")
+const RelativMass = new Formula("RelativMass (travelerMass, speed float64)", "mass*Y (Y=Lorentz factor)", "kg")
+const RelativMomentum = new Formula("RelativMomentum (travelerMomentum, speed float64)", "(travelerMomentum * speed)*Y (Y=Lorentz factor)", "kg*m/s")
+const RelativTime = new Formula("RelativTime (travelerTime, speed float64)", "travelerTime/Y (Y=Lorentz factor)", "s")
+const Resistance = new Formula("Resistance (resistance, currentIntensity float64)", "see 'JouleHeating'", "W")
 // Making some space
-const SelfCapacitance = new Formula("SelfCapacitance(charge, potential float64)", "charge/potential", "C/V")
-const SelfCapacitanceSphere = new Formula("SelfCapacitanceSphere(radius float64)", "4 * greekPi * Vacuum Premittivity constant * radius", "C/V")
-const Speed = new Formula("Speed(distance, time float64)", "distance/time", "m/s")
-const Time = new Formula("Time(distance, speed float64)", "distance/speed", "s")
-const Voltage = new Formula("Voltage(charge, distance1, distance2 float64)", "see 'ElectricPotentDiff'", "V")
-const Work = new Formula("Work(force, distance float64)", "force*distance", "J")
+const SelfCapacitance = new Formula("SelfCapacitance (charge, potential float64)", "charge/potential", "C/V")
+const SelfCapacitanceSphere = new Formula("SelfCapacitanceSphere (radius float64)", "4 * greekPi * Vacuum Premittivity constant * radius", "C/V")
+const Speed = new Formula("Speed (distance, time float64)", "distance/time", "m/s")
+const Time = new Formula("Time (distance, speed float64)", "distance/speed", "s")
+const Voltage = new Formula("Voltage (charge, distance1, distance2 float64)", "see 'ElectricPotentDiff'", "V")
+const Work = new Formula("Work (force, distance float64)", "force*distance", "J")
 
 const functionsList = [Acceleration, AngularFreq, CentripetalAccel, CentripetalForce, Density, DopplerCloser, 
     DopplerFarer, ElectricField, ElectricPotentDiff, ElectricPotentEnergyDiff, EnergyDens, EnergyDensity, EscapeSpeed,
@@ -169,7 +169,8 @@ window.onload = () => {
 function loadFormulas() {
     var documentationHtml = document.getElementById("functionsListHtml");
     for (var i=0; i<functionsList.length; i++) {
-        documentationHtml.innerHTML += "<div class='func'><div class='function-displayer' id="+">" + functionsList[i].name + 
+        documentationHtml.innerHTML += 
+        "<div class='func'><div class='function-displayer' id="+ String(functionsList[i].name).split(" ")[0] +">" + functionsList[i].name + 
         "</div><br/><span class='codes'>Formula: " + functionsList[i].formula + 
         "</span><br /><span class='codes'>Measurement Unit: " + functionsList[i].measurementUnit + "</span></div><br />";
     }
@@ -178,7 +179,8 @@ function loadFormulas() {
 function loadConstants() {
     var documentationHtml = document.getElementById("constantsListHtml")
     for (var i=0; i<constantsList.length; i++) {
-        documentationHtml.innerHTML += "<div class='func'><div class='function-displayer'>" + constantsList[i].name + 
+        documentationHtml.innerHTML += 
+        "<div class='func'><div class='function-displayer' id="+ constantsList[i].name +">" + constantsList[i].name + 
         "</div><br/><span class='codes'>Value: " + constantsList[i].value + " " + constantsList[i].measurementUnit + "</span></div><br />";
     }
 }
