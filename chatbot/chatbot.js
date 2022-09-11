f904cf5f5090f6c650fa66a6b881651af59ed148
Gabri432 <gabrielegatti432@gmail.com>
2022-09-11 17:13:15 +0200

    Adding border to the choices.

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,3 +1,6 @@
 const keywords = ["formula", "formulas", "constant", "constants", "contact", "someone", "help", "mathem", "library"];
 var input = document.getElementById("input-text").value;
 var userInput = input.toLowerCase();
+
+function searchSomething() {}
+function searchContacts()
